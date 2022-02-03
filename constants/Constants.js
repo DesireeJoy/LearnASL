cada26fd4c21a2ca4b867047ea84c13133715f3b
DesireeJoy <desireebradish@gmail.com>
2022-02-03 10:39:12 -0500

    First Commit

---
+++ constants/Constants.js
@@ -0,0 +1,138 @@
+export const reasons = [
+  {
+    id: 1,
+    reason: "Communicate under Water.",
+    desc: "Great for scuba diving or chatting with mermaids.",
+  },
+  {
+    id: 2,
+    reason: "Communicate with the Deaf community.",
+    desc:
+      "The Deaf community is unlike any other culture. Upon learning ASL you will see that they have their own unique histories, cultures and beliefs.",
+  },
+  {
+    id: 3,
+    reason: "Improve your non-verbal communication",
+    desc:
+      "ASL is a visual language that uses space, expressive movement and facial expressions to communicate extra information beyond the signs.",
+  },
+  {
+    id: 4,
+    reason: "Communicate in noisy situations.",
+    desc: "What?!? What?!! I can't hear you!",
+  },
+  {
+    id: 5,
+    reason: "Communicate in noise-less situations.",
+    desc: "When a librarian or movie theater usher shooshes you. Shhh!",
+  },
+  {
+    id: 6,
+    reason: "Learning a second Language stimulates the brain.",
+    desc:
+      "Learning a language boosts our concentration, listening and thinking skills!",
+  },
+  {
+    id: 7,
+    reason: "Great for those with Anxiety or Autism",
+    desc:
+      "ASL provides a way to communicate for those who have conditions that cause them to prefer to be non-verbal.",
+  },
+  {
+    id: 8,
+    reason: "Great for those with Anxiety or Autism",
+    desc:
+      "ASL provides a way to communicate for those who have conditions that cause them to prefer to be non-verbal.",
+  },
+  {
+    id: 9,
+    reason: "Enhance your CV",
+    desc: "ASL looks great on a resume.",
+  },
+  {
+    id: 10,
+    reason: "Improve your visualization skills",
+    desc:
+      "Check out the study linked below by geologist Michele Cooke. I know, a geologist sounds weird, but check it out!",
+    link:
+      "https://www.livescience.com/460-sign-language-improves-mental-abilities.html",
+  },
+];
+
+export const influencers = [
+  {
+    name: "Jon Urquhart",
+    desc:
+      "A son of a deafblind father Jon is a full time interpreter and an accesibility advocate.",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@drunkcrier/",
+  },
+  {
+    name: "Chrissy",
+    desc: "Deaf",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@chrissycanthearyou/",
+  },
+  {
+    name: "Otis Jones",
+    desc:
+      "A Black creator whose videos are mostly produced in his red and blue lit bedroom",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@slntwrlddd/",
+  },
+  {
+    name: "Phelan Conheady",
+    desc: "Deaf queer advocate",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://twitter.com/signingmango?lang=en",
+  },
+  {
+    name: "Jon Urquhart",
+    desc: "CODA",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@patrickandbigben/",
+  },
+  {
+    name: "Jacelyn Fincher",
+    desc: "Deaf",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@jacelyn.fincher/",
+  },
+  {
+    name: "Beth & Coop",
+    desc: "Hearing Mom & Deaf Son",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@beth_and_coop/",
+  },
+  {
+    name: "Becky Helen",
+    desc:
+      "Becky Mingins is a gay British (BSL users) creator with 716,000 followers on her TikTok account.",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@beckyyhelen/",
+  },
+  {
+    name: "@andyispleasants",
+    desc:
+      "Self-described Deaf artist and craniofacial advocate, Andy captivates audiences with his humorous skits and discussions about disability advocacy.",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "ttps://www.tiktok.com/@@andyispleasants",
+  },
+  {
+    name: "Dallin Smuin",
+    desc:
+      "A cochlear implant and hearing aid user, educates audiences about deafness and creates simple ASL tutorials",
+    pic: "drunkcrier",
+    linkType: "TikTok",
+    link: "https://www.tiktok.com/@dallinsmuin",
+  },
+];
---
+++ pages/index.js
@@ -0,0 +1,40 @@
+"use strict";
+import { reasons, influencers } from "../constants/Constants.js";
+
+function selectItem(reasons) {
+  let item = reasons[Math.round(Math.random() * (reasons.length - 1))];
+  return item;
+}
+
+let randomItem = selectItem(reasons);
+
+let cardData = {
+  title: randomItem.reason,
+  desc: randomItem.desc,
+  link: randomItem.link,
+};
+const titleEl = document.querySelector(".card__title");
+const descEl = document.querySelector(".card__desc");
+const linkEl = document.querySelector(".card__link");
+
+if (randomItem.link) {
+  linkEl.classList.add("visible");
+  linkEl.href = cardData.link;
+} else {
+}
+
+titleEl.textContent = cardData.title;
+descEl.textContent = cardData.desc;
+
+const getContainer = document.querySelector(".influencers__container");
+influencers.map((i) => {
+  var li = document.createElement("li");
+  getContainer.appendChild(li);
+  console.log("i");
+  let personHTML = `<IMG class='influencer__img' SRC='../images/${i.pic}.png' alt='influencer photo'/><div class='influencer__name'>${i.name}</div><div class='influencer__desc'>${i.desc}</div><a class='influencer__link' href='${i.link}'>${i.linkType}</a>`;
+  li.innerHTML = personHTML;
+  li.classList.add("influencers__item");
+});
+
+console.log(cardData);
+selectItem(reasons);
